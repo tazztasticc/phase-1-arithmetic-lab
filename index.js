f6a125b9c04d50c221b3d8418c8da98f2febef69
Tasif <taz181995@gmail.com>
2021-06-30 01:45:49 -0400

    commit tree 62dfa77c2f35354cc238af21d917fe341a593abb

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 6
+const num4 = 10
+const mod = num4 % num3;
+const max = 20;
